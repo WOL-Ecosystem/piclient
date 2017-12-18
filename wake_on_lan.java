ef19d188c2a6c31e44ba1569c093c4ac399b930c
geocfu <geocfuone@gmail.com>
2017-12-18 02:42:50 +0200

    New method will be implemented soon

--- wake_on_lan.java
+++
@@ -1,8 +0,0 @@
-public class wake_on_lan {
-    public static void main (String[] args) {
-        PublicIPAddress publicIP = new PublicIPAddress();
-        publicIP.getPublicIPAddress();
-        System.out.println(publicIP.toString());//testing purpuses
-        new local_ip();
-    }
-}
