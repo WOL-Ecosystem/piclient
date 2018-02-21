31844cd0b6fcb4fbd5e4b43fada6a6b43d502e8c
geocfu <geocfuone@gmail.com>
2018-02-21 17:32:12 +0200

    Correct handling of .properties variables

--- PropertiesReader.java
+++ PropertiesReader.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class PropertiesReader {
 
-    private static Map<String, String> propertiesCredentials = new HashMap<String, String>();
+    private String uuid, mac, username, token = "";
 
-    public static void main(String[] args) {
+    public PropertiesReader() {
 
     	Properties prop = new Properties();
     	InputStream input = null;
@@ -15,12 +15,12 @@ public class PropertiesReader {
 
     		// load a properties file
     		prop.load(input);
-
     		// get the property value
-            propertiesCredentials.put("uuid", prop.getProperty("uuid"));
-    		propertiesCredentials.put("mac", prop.getProperty("mac"));
-            propertiesCredentials.put("username", prop.getProperty("username"));
-            propertiesCredentials.put("token", prop.getProperty("token"));
+            this.uuid = prop.getProperty("uuid");
+            this.mac = prop.getProperty("mac");
+            this.username = prop.getProperty("username");
+            this.token = prop.getProperty("token");
+
 
     	} catch (IOException ex) {
     		ex.printStackTrace();
@@ -36,7 +36,19 @@ public class PropertiesReader {
     	}
     }
 
-    public Map<String, String> getProperties(){
-        return propertiesCredentials;
+    public String getUuid() {
+        return this.uuid;
+    }
+
+    public String getMac() {
+        return this.mac;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getToken() {
+        return this.token;
     }
 }
