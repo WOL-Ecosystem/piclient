f931deb616dedcbd47ba6d9174b84c4f9c002ea7
geocfu <geocfuone@gmail.com>
2018-02-23 02:52:11 +0200

    changed automation logic

--- Client.java
+++ Client.java
@@ -8,11 +8,12 @@ public class Client {
     public static void main (String[] args) {
         //read .properties
         PropertiesReader configuration = new PropertiesReader();
-        credentials.put("address", "https://wol.sht.gr/backend/ping");
-        credentials.put("uuid", configuration.getUuid());
-        credentials.put("mac", configuration.getMac());
+        credentials.put("postMethod", configuration.getHandshakeAddress());
+        credentials.put("pingAddress", configuration.getPingAddress());
         credentials.put("username", configuration.getUsername());
         credentials.put("token", configuration.getToken());
+        credentials.put("uuid", configuration.getUuid());
+        credentials.put("mac", configuration.getMac());
         //sent POST with the .properties credenntials
         POST POST = new POST(credentials);
         System.out.println(POST.toString()); // testing
