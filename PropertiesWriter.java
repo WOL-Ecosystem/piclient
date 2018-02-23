70f57b25104586016347c69031e45c37ea91544d
geocfu <geocfuone@gmail.com>
2018-02-23 02:51:55 +0200

    changed automation logic

--- PropertiesWriter.java
+++ PropertiesWriter.java
@@ -2,18 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class PropertiesWriter {
+
     public PropertiesWriter (Map<String, String> credentials) {
-        Properties config = new Properties();
+
+        Properties configuration = new Properties();
         OutputStream output = null;
         try {
-            output = new FileOutputStream("configuration.properties");
+            output = new FileOutputStream("configuration.properties", true);
             // set the properties value
-            config.setProperty("username", credentials.get("username"));
-            config.setProperty("token", credentials.get("token"));
-            config.setProperty("uuid", credentials.get("uuid"));
-            config.setProperty("mac", credentials.get("mac"));
+            /*
+            configuration.setProperty("handshakeAddress", credentials.get("handshakeAddress"));
+            configuration.setProperty("pingAddress", credentials.get("pingAddress"));
+            */
+            configuration.setProperty("controllerName", credentials.get("controllerName"));
+            configuration.setProperty("postMethod", credentials.get("postMethod"));
+            configuration.setProperty("username", credentials.get("username"));
+            configuration.setProperty("token", credentials.get("token"));
+            configuration.setProperty("uuid", credentials.get("uuid"));
+            configuration.setProperty("mac", credentials.get("mac"));
             // save properties to project root folder
-            config.store(output, null);
+            configuration.store(output, "PropertiesWriter");
 
         } catch (IOException ex) {
             ex.printStackTrace();
